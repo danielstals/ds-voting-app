688385ab33ac39d55fb75c0b6d2ac87fcae78d11
Daniel Stals <dstals@ilionx.com>
2022-12-20 17:54:30 +0100

    fix: Added comment to subscription component

--- src/app/core/helpers/subscription.helper.ts
+++ src/app/core/helpers/subscription.helper.ts
@@ -1,6 +1,9 @@
 import { Directive, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-
+/**
+ * This component can be implemented in other components in order to easily unsubscribe subscriptions
+ * when compoments get unloaded from the view. (Helps avoiding memory leaks)
+ */
 @Directive({ selector: '[subscriptionComponent]' })
 export class SubscriptionComponent implements OnDestroy {
   public subs: Subscription[] = [];
